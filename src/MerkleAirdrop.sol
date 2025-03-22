b84a3aecfdb2d4cb719199ee4060a6724e0df4c6
Syber88 <siyamgz88@gmail.com>
2025-03-22 22:20:31 +0200

    format

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.24;
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-
 contract MerkleAirdrop {
     using SafeERC20 for IERC20;
 
@@ -14,28 +13,24 @@ contract MerkleAirdrop {
     error MerkleAirdrop__AlreadyClaimed();
 
     bytes32 private immutable i_merkleRoot;
-    IERC20 private immutable i_airdropToken; 
-    mapping (address claimer => bool claimed) private s_hasClaimed;
-    
-    constructor (bytes32 merkleRoot, IERC20 airdropToken) {
+    IERC20 private immutable i_airdropToken;
+    mapping(address claimer => bool claimed) private s_hasClaimed;
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
         i_merkleRoot = merkleRoot;
         i_airdropToken = airdropToken;
-
     }
 
     function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
-        if (s_hasClaimed[account]){
+        if (s_hasClaimed[account]) {
             revert MerkleAirdrop__AlreadyClaimed();
         }
         bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount)))); // hashes twice to avoid collisions
-        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)){
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
             revert MerkleAirdrop__InvalidProof();
         }
         s_hasClaimed[account] = true;
         emit Claim(account, amount);
         i_airdropToken.safeTransfer(account, amount);
-
-
     }
-
 }
