68f20b1c42cfaaf2a0065495a2345f09818bccbb
Syber88 <siyamgz88@gmail.com>
2025-03-22 21:33:20 +0200

    partially done with clain function

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -1,6 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+
 contract MerkleAirdrop {
+
+    error MerkleAirdrop__InvalidProof();
+
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken; 
     
+    constructor (bytes32 merkleRoot, IERC20 airdropToken) {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+
+    }
+
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)){
+            revert MerkleAirdrop__InvalidProof()
+        }
+    }
+
 }
