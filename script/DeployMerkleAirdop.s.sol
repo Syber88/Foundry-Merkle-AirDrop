2fcd6f888226a6ad55c62d6a3ece2c899bbc3f71
Syber88 <siyamgz88@gmail.com>
2025-03-24 11:59:52 +0200

    refactor and format

--- script/DeployMerkleAirdop.s.sol
+++ script/DeployMerkleAirdop.s.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT 
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
@@ -10,17 +10,15 @@ contract DeployMerkleAirdop is Script {
     bytes32 private s_merkleRoot = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
     uint256 private s_amountToTransfer = 4 * (25 * 1e18);
 
-    function deployMerkleAirdrop() public returns(MerkleAirdrop, BagelToken){
+    function deployMerkleAirdrop() public returns (MerkleAirdrop, BagelToken) {
         vm.startBroadcast();
         BagelToken token = new BagelToken();
         MerkleAirdrop airdrop = new MerkleAirdrop(s_merkleRoot, IERC20(address(token)));
         token.mint(token.owner(), s_amountToTransfer);
         token.transfer(address(airdrop), s_amountToTransfer);
         vm.stopBroadcast();
+        return (airdrop, token);
     }
 
-
-    function run() external returns (MerkleAirdrop, BagelToken){
-
-    }
+    function run() external returns (MerkleAirdrop, BagelToken) {}
 }
