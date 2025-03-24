9d07132cfa1e1ae46b7eb5dfb80179d5fcdba5e9
Syber88 <siyamgz88@gmail.com>
2025-03-24 11:26:40 +0200

    completed test for users who can claim

--- test/MerkleAirdropTest.t.sol
+++ test/MerkleAirdropTest.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.24;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
 import {BagelToken} from "../src/BagelToken.sol";
 
@@ -12,6 +12,8 @@ contract MerkleAirdropTest is Test {
     bytes32 public ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
     address user;
     uint256 userPrivKey;
+    uint256 public AMOUNT = 25 * 1e18;
+    bytes32[] public PROOF = [0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a, 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576];
 
     function setUp() public {
         token = new BagelToken();
@@ -19,6 +21,12 @@ contract MerkleAirdropTest is Test {
         (user, userPrivKey) = makeAddrAndKey("user");
     }
     function testUsersCanClaim() public {
+        uint256 startingBalance = token.balanceOf(user);
+        vm.prank(user);
+        airdrop.claim(user, AMOUNT, PROOF);
 
+        uint256 endingBalance = token.balanceOf(user);
+        console.log("ending balance", user);
+        assertEq(endingBalance - startingBalance, AMOUNT);
     }
 }
