e1268fe1d774aff9cb461e41e7afd2f1fdcf2764
Syber88 <siyamgz88@gmail.com>
2025-03-25 21:27:11 +0200

    created function for checkin if signature is valid

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.24;
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract MerkleAirdrop is EIP712{
     using SafeERC20 for IERC20;
@@ -17,6 +18,8 @@ contract MerkleAirdrop is EIP712{
     bytes32 private immutable i_merkleRoot;
     IERC20 private immutable i_airdropToken;
     mapping(address claimer => bool claimed) private s_hasClaimed;
+
+    bytes32 private constant MESSAGE_TYPEHASH = keccak256("AirdropClain(address account, uint256 amount)");
 
     struct AirdropClaim {
         address account; 
@@ -51,6 +54,11 @@ contract MerkleAirdrop is EIP712{
         return _hashTypedDataV4(
             keccak256(abi.encode(MESSAGE_TYPEHASH, AirdropClaim({account: account, amount: amount})))
         );
+    }
+
+    function _isValidSignature(address account, bytes32 digest, uint8 v, bytes32 r, bytes32 s) internal pure returns (bool) {
+        (address actualSigner, , ) = ECDSA.tryRecover(digest, v, r, s);
+        return actualSigner == account;
     }
 
 
