995d37e543ed16e9a3e81c252367f088d04072ac
Syber88 <siyamgz88@gmail.com>
2025-03-25 22:43:22 +0200

    partially done with claim air drop function

--- script/Interact.s.sol
+++ script/Interact.s.sol
@@ -3,9 +3,24 @@ pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
 
 contract ClaimAirdrop is Script {
+    address public CLAIMING_ADDRESS = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    uint256 public CLAIMING_AMOUNT = 25 * 1e18;
+    bytes32 public proofOne = 0xd1445c931158119b00449ffcac3c947d028c0c359c34a6646d95962b3b55c6ad;
+    bytes32 public proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] public PROOF = [proofOne, proofTwo];
+
+    function claimAirdrop(address airdrop) public {
+        vm.startBroadcast();
+        MerkleAirdrop(airdrop).claim(CLAIMING_ADDRESS, CLAIMING_AMOUNT, PROOF, v, r, s);
+        vm.stopBroadcast();
+    }
+
+
     function run() external {
-        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment("MerkeleAirdrop", block.chainid);
+        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment("MerkleAirdrop", block.chainid);
+
     }
 }
