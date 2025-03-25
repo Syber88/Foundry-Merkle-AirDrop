455abb6f62ab485f0397d7e96f755752243eb574
Syber88 <siyamgz88@gmail.com>
2025-03-25 21:14:34 +0200

    initialised get message funciton

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -11,20 +11,32 @@ contract MerkleAirdrop {
 
     error MerkleAirdrop__InvalidProof();
     error MerkleAirdrop__AlreadyClaimed();
+    error MerkleAirdrop__InvalidSignature();
 
     bytes32 private immutable i_merkleRoot;
     IERC20 private immutable i_airdropToken;
     mapping(address claimer => bool claimed) private s_hasClaimed;
+
+    struct AirdropClaim {
+        address account; 
+        uint256 amount;
+    }
 
     constructor(bytes32 merkleRoot, IERC20 airdropToken) {
         i_merkleRoot = merkleRoot;
         i_airdropToken = airdropToken;
     }
 
-    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof, uint8 v, bytes32 r, bytes32 s) external {
         if (s_hasClaimed[account]) {
             revert MerkleAirdrop__AlreadyClaimed();
         }
+
+        if (!_isValidSignature(account, getMessage(account, amount), v, r, s)){
+            revert MerkleAirdrop__InvalidSignature();
+        }
+
+
         bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount)))); // hashes twice to avoid collisions
         if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
             revert MerkleAirdrop__InvalidProof();
@@ -32,6 +44,12 @@ contract MerkleAirdrop {
         s_hasClaimed[account] = true;
         emit Claim(account, amount);
         i_airdropToken.safeTransfer(account, amount);
+    }
+
+    function getMessage(address account, uint256 amount) public view returns (bytes32) {
+        return _hashTypedDataV4{
+            keccak256(abi.encode(MESSAGE_TYPEHASH, account, amount))
+        }
     }
 
     function getMerkleRoot() external view returns (bytes32) {
