b4742ba53ea68c505150eff97ae8a5d8d10a7cc7
Syber88 <siyamgz88@gmail.com>
2025-03-26 11:39:24 +0200

    fixed issue of the contracts not returning proper addresses

--- script/DeployMerkleAirdrop.s.sol
+++ script/DeployMerkleAirdrop.s.sol
@@ -6,7 +6,7 @@ import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
 import {BagelToken} from "../src/BagelToken.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract DeployMerkleAirdop is Script {
+contract DeployMerkleAirdrop is Script {
     bytes32 private s_merkleRoot = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
     uint256 private s_amountToTransfer = 4 * (25 * 1e18);
 
@@ -15,10 +15,12 @@ contract DeployMerkleAirdop is Script {
         BagelToken token = new BagelToken();
         MerkleAirdrop airdrop = new MerkleAirdrop(s_merkleRoot, IERC20(address(token)));
         token.mint(token.owner(), s_amountToTransfer);
-        token.transfer(address(airdrop), s_amountToTransfer);
+        IERC20(token).transfer(address(airdrop), s_amountToTransfer);
         vm.stopBroadcast();
         return (airdrop, token);
     }
 
-    function run() external returns (MerkleAirdrop, BagelToken) {}
+    function run() external returns (MerkleAirdrop, BagelToken) {
+        return deployMerkleAirdrop();
+    }
 }
