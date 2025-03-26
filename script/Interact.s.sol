3fd6fa0c590615d45e7369c71883cb7d19d83192
Syber88 <siyamgz88@gmail.com>
2025-03-26 11:55:07 +0200

    initialised function to split up signature from signing a transaction

--- script/Interact.s.sol
+++ script/Interact.s.sol
@@ -11,11 +11,18 @@ contract ClaimAirdrop is Script {
     bytes32 public proofOne = 0xd1445c931158119b00449ffcac3c947d028c0c359c34a6646d95962b3b55c6ad;
     bytes32 public proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
     bytes32[] public PROOF = [proofOne, proofTwo];
+    bytes private SIGNATURE = hex"0xa5cdeb2b32e45e45875860ade9c2e833e705104ce903eeb055f0b0a04cb9c72103a7e4e4186b4f427704b2cd29083ac8e2f5617bca55936644dee71664ce08e31c"
 
     function claimAirdrop(address airdrop) public {
         vm.startBroadcast();
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(SIGNATURE);
         MerkleAirdrop(airdrop).claim(CLAIMING_ADDRESS, CLAIMING_AMOUNT, PROOF, v, r, s);
         vm.stopBroadcast();
+    }
+
+    function splitSignature(bytes memory sig) public returns (uint8 v, bytes32 r, bytes32 s) {
+        require(sig.length == 65, "invalid signature length");
+        assembly
     }
 
     function run() external {
